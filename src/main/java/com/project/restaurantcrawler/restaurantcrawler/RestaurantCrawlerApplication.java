d4ceb86c1449edb4a151b403aa855402f6f644ca
huigyun <hydong15@ajou.ac.kr>
2019-12-09 14:36:49 +0900

    Add spring boot project

---
+++ src/main/java/com/project/restaurantcrawler/restaurantcrawler/RestaurantCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.project.restaurantcrawler.restaurantcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/restaurantcrawler/restaurantcrawler/RestaurantCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.restaurantcrawler.restaurantcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
